8dc87cc528db1eb66792cbb2979f52095438efe9
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2024-05-24 16:05:31 -0300

    feat: added form validation
    #Added form validation and style

--- src/app/features/add-product/add-product/add-product.component.ts
+++ src/app/features/add-product/add-product/add-product.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-
 import { Product } from '../../../core/models/product';
 import { CatalogService } from '../../../core/services/catalog.service';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrl: './add-product.component.scss'
+  styleUrls: ['./add-product.component.scss']
 })
 export class AddProductComponent {
 
@@ -26,25 +24,49 @@ export class AddProductComponent {
     this.productId = this.route.snapshot.params["id"];
 
     this.productForm = new FormGroup({
-      title: new FormControl(this.productToUpdate?.name || "Título Padrão"),
-      description: new FormControl(this.productToUpdate?.description),
-      price: new FormControl(this.productToUpdate?.price),
-      quantity: new FormControl(this.productToUpdate?.quantity),
-      imageUrl: new FormControl(this.productToUpdate?.imageUrl)
-      
-  });
+      title: new FormControl(this.productToUpdate?.name, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50)
+      ]),
+      description: new FormControl(this.productToUpdate?.description, [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(500)
+      ]),
+      price: new FormControl(this.productToUpdate?.price, [
+        Validators.required,
+        Validators.min(0.01)
+      ]),
+      quantity: new FormControl(this.productToUpdate?.quantity, [
+        Validators.required,
+        Validators.min(0),
+        Validators.pattern("^[0-9]*$")  // Ensure it's an integer
+      ]),
+      imageUrl: new FormControl(this.productToUpdate?.imageUrl, [
+        Validators.required,
+        Validators.pattern('https?://.+')
+      ])
+    });
+  }
+
+  isFieldInvalid(field: string): boolean {
+    const control = this.productForm.get(field);
+    return control ? control.invalid && (control.dirty || control.touched) : false;
   }
 
   formSubmit() {
-    let productData: Product = this.productForm.value;
-
-    if(this.productId) {
-      this.catalogService.updateProduct(productData);
+    if (this.productForm.invalid) {
       return;
     }
 
-    this.catalogService.addProduct(productData);
-    this.productForm.reset();
-  }
+    let productData: Product = this.productForm.value;
 
+    if (this.productId) {
+      this.catalogService.updateProduct(productData);
+    } else {
+      this.catalogService.addProduct(productData);
+      this.productForm.reset();
+    }
+  }
 }
