25830aced8a41f162c370ed0a19065417581b3dc
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2024-05-23 21:31:53 -0300

    feat: created add-product
    #Created add-product component, routes working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'catalog', pathMatch: 'full' },
   { path: 'catalog', loadChildren: () => import('./features/catalog/catalog.module').then(m => m.CatalogModule) },
   { path: 'cart', loadChildren: () => import('./features/cart/cart.module').then(m => m.CartModule) },
+  { path: 'add-product', loadChildren: () => import('./features/add-product/add-product.module').then(m => m.AddProductModule) },
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
   { path: 'product/:id', loadChildren: () => import('./features/product-detail/product-detail.module').then(m => m.ProductDetailModule) },
   { path: '**', redirectTo: 'catalog' }
